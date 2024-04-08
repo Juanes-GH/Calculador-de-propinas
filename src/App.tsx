b6ba318a5bb8ba7085c6f6ad4a77f93f77359efe
JuanesGH <jheras@2amigos.us>
2024-04-08 13:59:02 +0200

    db type setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-
+import { useState, useEffect } from "react"
+import { menuItems } from "./data/db"
 
 function App() {
 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    setData(menuItems)
+  },[])
+
   return (
     <>
-      <h1 className="uppercase text-bold text-4xl text-cyan-700">Hola Mundo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas y Consumo</h1>
+      </header>
+      
+      <main className="">
+          <h2>Menu</h2>
+      </main>
+
     </>
   )
 }
